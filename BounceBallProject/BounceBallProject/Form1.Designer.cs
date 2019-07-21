8e46006564ab65029fa9cca5181083df6a6326b9
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-07-21 16:53:57 +0300

    Add files via upload

---
+++ BounceBallProject/BounceBallProject/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BounceBallProject
+{
+    partial class BounceBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Jumpbtn = new System.Windows.Forms.Button();
+            this.AddBallbtrn = new System.Windows.Forms.Button();
+            this.RemoveBallbtn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Jumpbtn
+            // 
+            this.Jumpbtn.Font = new System.Drawing.Font("Old English Text MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jumpbtn.Location = new System.Drawing.Point(-2, -1);
+            this.Jumpbtn.Name = "Jumpbtn";
+            this.Jumpbtn.Size = new System.Drawing.Size(196, 52);
+            this.Jumpbtn.TabIndex = 0;
+            this.Jumpbtn.Text = "Jump";
+            this.Jumpbtn.UseVisualStyleBackColor = true;
+            this.Jumpbtn.Click += new System.EventHandler(this.Jumpbtn_Click);
+            // 
+            // AddBallbtrn
+            // 
+            this.AddBallbtrn.Font = new System.Drawing.Font("Old English Text MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddBallbtrn.Location = new System.Drawing.Point(186, -1);
+            this.AddBallbtrn.Name = "AddBallbtrn";
+            this.AddBallbtrn.Size = new System.Drawing.Size(226, 52);
+            this.AddBallbtrn.TabIndex = 1;
+            this.AddBallbtrn.Text = "AddNewBall";
+            this.AddBallbtrn.UseVisualStyleBackColor = true;
+            this.AddBallbtrn.Click += new System.EventHandler(this.AddBallbtrn_Click);
+            // 
+            // RemoveBallbtn
+            // 
+            this.RemoveBallbtn.Font = new System.Drawing.Font("Old English Text MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveBallbtn.Location = new System.Drawing.Point(406, -1);
+            this.RemoveBallbtn.Name = "RemoveBallbtn";
+            this.RemoveBallbtn.Size = new System.Drawing.Size(208, 52);
+            this.RemoveBallbtn.TabIndex = 2;
+            this.RemoveBallbtn.Text = "RemoveBall";
+            this.RemoveBallbtn.UseVisualStyleBackColor = true;
+            this.RemoveBallbtn.Click += new System.EventHandler(this.RemoveBallbtn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BounceBallProject.Properties.Resources.untitled_png12;
+            this.pictureBox2.Location = new System.Drawing.Point(381, 238);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(217, 182);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BounceBallProject.Properties.Resources.orig1;
+            this.pictureBox1.Image = global::BounceBallProject.Properties.Resources.untitled;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 238);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // BounceBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RemoveBallbtn);
+            this.Controls.Add(this.AddBallbtrn);
+            this.Controls.Add(this.Jumpbtn);
+            this.Name = "BounceBall";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.BounceBall_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Jumpbtn;
+        private System.Windows.Forms.Button AddBallbtrn;
+        private System.Windows.Forms.Button RemoveBallbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ BounceBallProject/BounceBallProject/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BounceBallProject
+{
+    public partial class BounceBall : Form
+    {
+        public BounceBall()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+        }
+
+        int dx = 1;
+        int dy = 1;
+       
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Jumpbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    Action a = () => { pictureBox1.Location = new Point(pictureBox1.Location.X + dx, pictureBox1.Location.Y + dy); };
+                    pictureBox1.BeginInvoke(a);
+                    if (pictureBox1.Location.X + dx <= 0)
+                        dx = 1;
+                    if (pictureBox1.Location.Y + dy <= 0 + Jumpbtn.Height)
+                        dy = 1;
+                    if (pictureBox1.Location.X + dx >= ClientSize.Width - pictureBox1.Width)
+                        dx = -1;
+                    if (pictureBox1.Location.Y + dy >= ClientSize.Height - pictureBox1.Height)
+                        dy = -1;
+                    Thread.Sleep(10);
+                }
+
+            });
+        }
+
+        private void AddBallbtrn_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            
+           
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Action b = () => { pictureBox2.Location = new Point(pictureBox2.Location.X + dx, pictureBox2.Location.Y + dy); };
+                    pictureBox2.BeginInvoke(b);
+                    if (pictureBox2.Location.X  + dx <= 0)
+                        dx = 1;
+                    if (pictureBox2.Location.Y + dy <= 0 + AddBallbtrn.Height )
+                        dy = 1;
+                    if (pictureBox2.Location.X + dx >= ClientSize.Width - pictureBox2.Width)
+                        dx = -1;
+                    if (pictureBox2.Location.Y + dy >= ClientSize.Height - pictureBox2.Height)
+                        dy = -1;
+                    Thread.Sleep(20);
+                }
+
+            });
+           
+        }
+
+        private void RemoveBallbtn_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+        }
+
+        private void BounceBall_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
